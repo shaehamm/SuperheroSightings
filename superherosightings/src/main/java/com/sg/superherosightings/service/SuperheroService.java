26497cdf6ef2a0066bd2ee39a8ec98b11443bc0d
shaehamm <shaehamm@gmail.com>
2020-08-17 10:52:15 -0500

    organized service layer

--- superherosightings/src/main/java/com/sg/superherosightings/service/SuperheroService.java
+++ superherosightings/src/main/java/com/sg/superherosightings/service/SuperheroService.java
@@ -33,20 +33,9 @@ public class SuperheroService {
     @Autowired
     SightingDao sightDao;
 
+    //hero dao
     public List<Hero> getAllHeroes() {
         return heroDao.getAllHeroes();
-    }
-
-    public List<Location> getAllLocations() {
-        return locationDao.getAllLocations();
-    }
-
-    public List<Org> getAllOrgs() {
-        return orgDao.getAllOrgs();
-    }
-
-    public List<Quirk> getAllQuirks() {
-        return quirkDao.getAllQuirks();
     }
 
     public void addHero(Hero toAdd) throws NullHeroDataException {
@@ -57,27 +46,6 @@ public class SuperheroService {
         return heroDao.getHeroById(id);
     }
 
-    public List<Sighting> getSightingsForHero(Hero hero) {
-        return sightDao.getSightingsForHero(hero);
-    }
-
-    public List<Org> getOrgsForHero(int id) {
-        return orgDao.getOrgsForHero(id);
-    }
-
-
-    public Quirk getQuirkById(int id) {
-        return quirkDao.getQuirkById(id);
-    }
-
-    public void updateOrg(Org org) {
-        orgDao.updateOrg(org);
-    }
-
-    public Org getOrgById(Integer id) {
-        return orgDao.getOrgById(id);
-    }
-
     public void updateHero(Hero edited) throws NullHeroDataException {
         heroDao.updateHero(edited);
     }
@@ -86,38 +54,17 @@ public class SuperheroService {
         heroDao.deleteHeroById(id);
     }
 
-    public void uniqueHeroNameCheck(String name, BindingResult valResult) {
-        if (getAllHeroes().stream().anyMatch(hero -> hero.getName().equalsIgnoreCase(name))) {
-            FieldError error = new FieldError("hero", "name",
-                    "Hero name already exists.");
-            valResult.addError(error);
-        }
+    public List<Hero> getHeroesForOrg(Org org) throws NullOrganizationDataException {
+        return heroDao.getHeroesForOrg(org);
     }
 
-    public void uniqueHeroNameCheck(String name, int id, BindingResult valResult) {
-        if (getAllHeroes().stream().anyMatch(hero -> hero.getName().equalsIgnoreCase(name) && hero.getId() != id)) {
-            FieldError error = new FieldError("hero", "name",
-                    "Hero name already exists.");
-            valResult.addError(error);
-        }
+    public List<Hero> getHeroesForQuirk(Quirk quirk) throws NullQuirkDataException {
+        return heroDao.getHeroesForQuirk(quirk);
     }
 
-    public void updateOrgsForHero(Hero edited, Integer[] orgIds) {
-        for (Org org : getOrgsForHero(edited.getId())) {
-            //removes orgs if they are no longer selected
-            if (Arrays.stream(orgIds).anyMatch(id -> id != org.getId())) {
-                org.getHeroes().remove(edited);
-                updateOrg(org);
-            }
-        }
-        for (Integer orgId : orgIds) {
-            Org toUpdate = getOrgById(orgId);
-            //check if hero is added to org already (add new hero if not already a member)
-            if (toUpdate.getHeroes().stream().allMatch(h -> h.getId() != edited.getId())) {
-                toUpdate.getHeroes().add(edited);
-            }
-            updateOrg(toUpdate);
-        }
+    //location dao
+    public List<Location> getAllLocations() {
+        return locationDao.getAllLocations();
     }
 
     public void addLocation(Location toAdd) throws NullLocationDataException {
@@ -128,10 +75,6 @@ public class SuperheroService {
         return locationDao.getLocationById(id);
     }
 
-    public List<Sighting> getSightingsByLocation(Location location) {
-        return sightDao.getSightingsByLocation(location);
-    }
-
     public void updateLocation(Location location) throws NullLocationDataException {
         locationDao.updateLocation(location);
     }
@@ -140,71 +83,38 @@ public class SuperheroService {
         locationDao.deleteLocationById(id);
     }
 
-    public void uniqueLocationNameCheck(String name, BindingResult valResult) {
-        if (getAllLocations().stream().anyMatch(location -> location.getName().equalsIgnoreCase(name))) {
-            FieldError error = new FieldError("location", "name",
-                    "Location already exists.");
-            valResult.addError(error);
-        }
+    //org dao
+    public List<Org> getAllOrgs() {
+        return orgDao.getAllOrgs();
     }
 
-    public void uniqueLocationNameCheck(String name, int id, BindingResult valResult) {
-        if (getAllLocations().stream().anyMatch(location -> location.getName().equalsIgnoreCase(name) &&
-                location.getId() != id)) {
-            FieldError error = new FieldError("location", "name",
-                    "Location already exists.");
-            valResult.addError(error);
-        }
+    public void updateOrg(Org org) {
+        orgDao.updateOrg(org);
     }
 
-    public List<Sighting> getLatestSightings(int count) {
-        return sightDao.getLatestSightings(count);
+    public Org getOrgById(Integer id) {
+        return orgDao.getOrgById(id);
+    }
+
+    public List<Org> getOrgsForHero(int id) {
+        return orgDao.getOrgsForHero(id);
     }
 
     public void addOrg(Org toAdd) {
         orgDao.addOrg(toAdd);
     }
 
-    public List<Hero> getHeroesForOrg(Org org) throws NullOrganizationDataException {
-        return heroDao.getHeroesForOrg(org);
-    }
-
     public void deleteOrgById(Integer id) {
         orgDao.deleteOrgById(id);
     }
 
-    public List<Hero> selectedHeroCheck(Integer[] heroIds, BindingResult valResult) {
-        List<Hero> toReturn = new ArrayList<>();
-        if (heroIds != null) {
-            for (Integer id : heroIds) {
-                toReturn.add(getHeroById(id));
-            }
-        } else {
-            FieldError error = new FieldError("org", "heroes",
-                    "Must include one hero.");
-            valResult.addError(error);
-        }
-        return toReturn;
+    //quirk dao
+    public List<Quirk> getAllQuirks() {
+        return quirkDao.getAllQuirks();
     }
 
-    public void uniqueOrgNameCheck(String name, BindingResult valResult) {
-        if (getAllOrgs().stream().anyMatch(org -> org.getName().equalsIgnoreCase(name))) {
-            FieldError error = new FieldError("org", "name",
-                    "Organization already exists.");
-            valResult.addError(error);
-        }
-    }
-
-    public void uniqueOrgNameCheck(String name, int id, BindingResult valResult) {
-        if (getAllOrgs().stream().anyMatch(org -> org.getName().equalsIgnoreCase(name) && org.getId() != id)) {
-            FieldError error = new FieldError("org", "name",
-                    "Organization already exists.");
-            valResult.addError(error);
-        }
-    }
-
-    public List<Hero> getHeroesForQuirk(Quirk quirk) throws NullQuirkDataException {
-        return heroDao.getHeroesForQuirk(quirk);
+    public Quirk getQuirkById(int id) {
+        return quirkDao.getQuirkById(id);
     }
 
     public void deleteQuirkById(Integer id) {
@@ -219,22 +129,7 @@ public class SuperheroService {
         quirkDao.addQuirk(toAdd);
     }
 
-    public void uniqueQuirkNameCheck(String name, BindingResult valResult) {
-        if (getAllQuirks().stream().anyMatch(quirk -> quirk.getName().equalsIgnoreCase(name))) {
-            FieldError error = new FieldError("quirk", "name",
-                    "Quirk name already exists.");
-            valResult.addError(error);
-        }
-    }
-
-    public void uniqueQuirkNameCheck(String name, int id, BindingResult valResult) {
-        if (getAllQuirks().stream().anyMatch(quirk -> quirk.getName().equalsIgnoreCase(name) && quirk.getId() != id)) {
-            FieldError error = new FieldError("quirk", "name",
-                    "Quirk name already exists.");
-            valResult.addError(error);
-        }
-    }
-
+    //sighting dao
     public List<Sighting> getAllSightings() {
         return sightDao.getAllSightings();
     }
@@ -254,4 +149,117 @@ public class SuperheroService {
     public void deleteSightingById(Integer id) {
         sightDao.deleteSightingById(id);
     }
+
+    public List<Sighting> getSightingsForHero(Hero hero) {
+        return sightDao.getSightingsForHero(hero);
+    }
+
+    public List<Sighting> getSightingsByLocation(Location location) {
+        return sightDao.getSightingsByLocation(location);
+    }
+
+    public List<Sighting> getLatestSightings(int count) {
+        return sightDao.getLatestSightings(count);
+    }
+
+    //unique checks
+    public void uniqueHeroNameCheck(String name, BindingResult valResult) {
+        if (getAllHeroes().stream().anyMatch(hero -> hero.getName().equalsIgnoreCase(name))) {
+            FieldError error = new FieldError("hero", "name",
+                    "Hero name already exists.");
+            valResult.addError(error);
+        }
+    }
+
+    public void uniqueHeroNameCheck(String name, int id, BindingResult valResult) {
+        if (getAllHeroes().stream().anyMatch(hero -> hero.getName().equalsIgnoreCase(name) && hero.getId() != id)) {
+            FieldError error = new FieldError("hero", "name",
+                    "Hero name already exists.");
+            valResult.addError(error);
+        }
+    }
+
+    public void uniqueLocationNameCheck(String name, BindingResult valResult) {
+        if (getAllLocations().stream().anyMatch(location -> location.getName().equalsIgnoreCase(name))) {
+            FieldError error = new FieldError("location", "name",
+                    "Location already exists.");
+            valResult.addError(error);
+        }
+    }
+
+    public void uniqueLocationNameCheck(String name, int id, BindingResult valResult) {
+        if (getAllLocations().stream().anyMatch(location -> location.getName().equalsIgnoreCase(name) &&
+                location.getId() != id)) {
+            FieldError error = new FieldError("location", "name",
+                    "Location already exists.");
+            valResult.addError(error);
+        }
+    }
+
+    public void uniqueOrgNameCheck(String name, BindingResult valResult) {
+        if (getAllOrgs().stream().anyMatch(org -> org.getName().equalsIgnoreCase(name))) {
+            FieldError error = new FieldError("org", "name",
+                    "Organization already exists.");
+            valResult.addError(error);
+        }
+    }
+
+    public void uniqueOrgNameCheck(String name, int id, BindingResult valResult) {
+        if (getAllOrgs().stream().anyMatch(org -> org.getName().equalsIgnoreCase(name) && org.getId() != id)) {
+            FieldError error = new FieldError("org", "name",
+                    "Organization already exists.");
+            valResult.addError(error);
+        }
+    }
+
+    public void uniqueQuirkNameCheck(String name, BindingResult valResult) {
+        if (getAllQuirks().stream().anyMatch(quirk -> quirk.getName().equalsIgnoreCase(name))) {
+            FieldError error = new FieldError("quirk", "name",
+                    "Quirk name already exists.");
+            valResult.addError(error);
+        }
+    }
+
+    public void uniqueQuirkNameCheck(String name, int id, BindingResult valResult) {
+        if (getAllQuirks().stream().anyMatch(quirk -> quirk.getName().equalsIgnoreCase(name) && quirk.getId() != id)) {
+            FieldError error = new FieldError("quirk", "name",
+                    "Quirk name already exists.");
+            valResult.addError(error);
+        }
+    }
+
+    //update orgs from hero side
+    public void updateOrgsForHero(Hero edited, Integer[] orgIds) {
+        for (Org org : getOrgsForHero(edited.getId())) {
+            //removes orgs if they are no longer selected
+            if (Arrays.stream(orgIds).anyMatch(id -> id != org.getId())) {
+                org.getHeroes().remove(edited);
+                updateOrg(org);
+            }
+        }
+        for (Integer orgId : orgIds) {
+            Org toUpdate = getOrgById(orgId);
+            //check if hero is added to org already (add new hero if not already a member)
+            if (toUpdate.getHeroes().stream().allMatch(h -> h.getId() != edited.getId())) {
+                toUpdate.getHeroes().add(edited);
+            }
+            updateOrg(toUpdate);
+        }
+    }
+
+    //check if heroes are selected for an org
+    public List<Hero> selectedHeroCheck(Integer[] heroIds, BindingResult valResult) {
+        List<Hero> toReturn = new ArrayList<>();
+        if (heroIds != null) {
+            for (Integer id : heroIds) {
+                toReturn.add(getHeroById(id));
+            }
+        } else {
+            FieldError error = new FieldError("org", "heroes",
+                    "Must include one hero.");
+            valResult.addError(error);
+        }
+        return toReturn;
+    }
+
 }
